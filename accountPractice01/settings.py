f3b40691865bf89237e05d78322e72cbb484868b
Mosi-azizi <kassit.azizi@gmail.com>
2022-01-30 13:52:13 +0330

    send welcome email

--- accountPractice01/settings.py
+++ accountPractice01/settings.py
@@ -135,7 +135,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'kassit.azizi@gmail.com'
-EMAIL_HOST_PASSWORD = 'qofzqwpztpfhfrfq'
+EMAIL_HOST_PASSWORD = ''
 
 
 # Static files (CSS, JavaScript, Images)
