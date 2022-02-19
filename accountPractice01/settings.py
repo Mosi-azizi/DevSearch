f2d840dee466a3a3dd277bdf09f3805edff48b0b
Mosi_Azizi <kassit.azizi@gmail.com>
2022-02-19 11:05:20 +0330

    Connecting to Postgres DB

--- accountPractice01/settings.py
+++ accountPractice01/settings.py
@@ -134,13 +134,24 @@ WSGI_APPLICATION = 'accountPractice01.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
